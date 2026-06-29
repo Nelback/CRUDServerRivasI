f28e59e55aeaae0c211cb76f711af3d14deafb0d
Nelback <luisiitoto@gmail.com>
2026-06-29 10:28:15 -0500

    cookies II

--- app.js
+++ app.js
@@ -6,10 +6,11 @@ const PORT = 3000
 const loginRoute = require("./src/routes/loginRoute")
 const getRoutes = require("./src/routes/getRoutes")
 const cors = require("cors")
+app.set('trust proxy', 1);
 app.use(cors({origin:"https://crud-client-rivas.vercel.app", methods:['GET', 'POST', 'DELETE', 'PUT'], credentials:true,}))
 app.use(cookieParser())
 app.use(express.json())
-app.set('trust proxy', 1);
+
 
 
 
